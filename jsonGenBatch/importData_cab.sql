beaa28e10a0885e9fb0ec0c4fca98490d6b50ef9
jocelynsun13 <40777473+jocelynsun13@users.noreply.github.com>
2018-08-12 17:43:01 -0400

    Delete importData_cab.sql

--- jsonGenBatch/importData_cab.sql
+++
@@ -1,4 +0,0 @@
-copy cab_busy from 'dir/cabbusy.txt' with delimiter ',' null '' csv header;
-copy cab_list from 'dir/cablist.txt' with delimiter ',' null '' csv header;
-copy cab_state from 'dir/cabstate.txt' with delimiter ',' null '' csv header;
-copy cab_loc from 'dir/cabloc.txt' with delimiter ',' null '' csv header;
