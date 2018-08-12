6d06c49cb84a2a121304c13d582d0e437ffff841
jocelynsun13 <40777473+jocelynsun13@users.noreply.github.com>
2018-08-12 17:42:19 -0400

    Delete createTables_cab.sql

--- jsonGenBatch/createTables_cab.sql
+++
@@ -1,4 +0,0 @@
-create table cab_busy (t integer, revenue integer, cost numeric);
-create table cab_list (t integer, cab_id varchar(4), status integer);
-create table cab_state (t integer, state varchar(50), moved varchar(50));
-create table cab_loc(t integer, lat numeric, lon numeric, distance numeric);
